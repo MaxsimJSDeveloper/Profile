2defa2a6967dd380817d01f10f7032b8e751b02d
Maksym <golovkomaksim852@gmail.com>
2024-04-22 17:18:33 +0300

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,37 @@
+import clsx from "clsx";
+import css from "./Profile.module.css";
+
 export const Profile = ({ image, name, tag, location, stats }) => (
-  <div>
-    <div>
-      <img src={image} alt="User avatar" />
-      <p>{name}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
+  <div className={clsx(css.profileWrapper)}>
+    <div className={clsx(css.infoWrapper)}>
+      <div className={clsx(css.imgWrapper)}>
+        <img
+          className={clsx(css.userPhoto)}
+          src={image}
+          alt="User avatar"
+          height="250"
+          width="250"
+        />
+      </div>
+      <p className={clsx(css.userName)}>{name}</p>
+      <div className={clsx(css.userInfo)}>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
     </div>
 
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{stats.followers}</span>
+    <ul className={clsx(css.stats)}>
+      <li className={clsx(css.statsItem)}>
+        <span className={clsx(css.statsName)}>Followers</span>
+        <span className={clsx(css.statsQuantity)}>{stats.followers}</span>
       </li>
-      <li>
-        <span>Views</span>
-        <span>{stats.views}</span>
+      <li className={clsx(css.statsItem)}>
+        <span className={clsx(css.statsName)}>Views</span>
+        <span className={clsx(css.statsQuantity)}>{stats.views}</span>
       </li>
-      <li>
-        <span>Likes</span>
-        <span>{stats.likes}</span>
+      <li className={clsx(css.statsItem)}>
+        <span className={clsx(css.statsName)}>Likes</span>
+        <span className={clsx(css.statsQuantity)}>{stats.likes}</span>
       </li>
     </ul>
   </div>
