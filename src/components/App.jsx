cd7f98c7db7c2afa8a97aefe2a3ec6625a0a58dc
Maksym <golovkomaksim852@gmail.com>
2024-04-23 21:53:50 +0300

    destructurisetion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,7 @@ import transactions from "../transactions.json";
 export default function App() {
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
+      <Profile userData={userData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,21 @@
 import clsx from "clsx";
 import css from "./Profile.module.css";
 
-export const Profile = ({ image, name, tag, location, stats }) => (
+export const Profile = ({
+  userData: {
+    avatar,
+    name,
+    tag,
+    location,
+    stats: { followers, views, likes },
+  },
+}) => (
   <div className={clsx(css.profileWrapper)}>
     <div className={clsx(css.infoWrapper)}>
       <div className={clsx(css.imgWrapper)}>
         <img
           className={clsx(css.userPhoto)}
-          src={image}
+          src={avatar}
           alt="User avatar"
           height="250"
           width="250"
@@ -23,15 +31,15 @@ export const Profile = ({ image, name, tag, location, stats }) => (
     <ul className={clsx(css.stats)}>
       <li className={clsx(css.statsItem)}>
         <span className={clsx(css.statsName)}>Followers</span>
-        <span className={clsx(css.statsQuantity)}>{stats.followers}</span>
+        <span className={clsx(css.statsQuantity)}>{followers}</span>
       </li>
       <li className={clsx(css.statsItem)}>
         <span className={clsx(css.statsName)}>Views</span>
-        <span className={clsx(css.statsQuantity)}>{stats.views}</span>
+        <span className={clsx(css.statsQuantity)}>{views}</span>
       </li>
       <li className={clsx(css.statsItem)}>
         <span className={clsx(css.statsName)}>Likes</span>
-        <span className={clsx(css.statsQuantity)}>{stats.likes}</span>
+        <span className={clsx(css.statsQuantity)}>{likes}</span>
       </li>
     </ul>
   </div>
